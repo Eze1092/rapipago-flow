4c831d2cce33588db075bf832b343719756bd308
Eze1092 <ezequielc31092@gmail.com>
2026-09-22 20:40:30 -0300

    Update cajeros_atm.tsx

--- src/routes/_authenticated/cajeros_atm.tsx
+++ src/routes/_authenticated/cajeros_atm.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router';
+
 import { useState, useEffect } from 'react';
 import { supabase } from '@/integrations/supabase/client';
 import { Plus, Trash2, Landmark, RefreshCw } from 'lucide-react';
